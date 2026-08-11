92e6d4f7dc0d594df3814144babbf394da71eaea
Vinod Karmodiya <karmodiya.vinod@gmail.com>
2026-08-11 18:50:26 +0530

    further update

--- 07_rag_tool_intrupt_agent.py
+++ 07_rag_tool_intrupt_agent.py
@@ -23,14 +23,10 @@ prompt = ChatPromptTemplate.from_messages([
 
 chain = prompt | get_llm()
 
-def showState(source:str, config: RunnableConfig):
-    return
-    configurable = config.get('metadata')
-    print(f'Source: {source}, Configuration: {configurable}')
-    
+def is_approved(message: str):
+    return str(message).strip().lower() in ("approve","approved","yes"," go ahead","good")    
 
 def query(state:State, config: RunnableConfig):
-    showState('query', config)
     query = input('Ask: ')
     if query == 'exit':
         raise ValueError("Existing...")
@@ -38,15 +34,10 @@ def query(state:State, config: RunnableConfig):
     return {"messages": [("user", query)]}
 
 def code_develop(state:State, config: RunnableConfig):
-    showState('code_develop',config)
-    print('code_develop.')
-    print(state['messages'])
-
     response = chain.invoke(state['messages'])
     return  {"messages": [response]}
 
 def human_review(state:State, config: RunnableConfig):
-    showState('human_review',config)
     code_generated = state['messages'][-1].text
     print('START Code to review'+ '*' * 50)
     print(code_generated)
@@ -54,18 +45,12 @@ def human_review(state:State, config: RunnableConfig):
     
     decision = interrupt({'draft': 'Instructions: Reply "Approve" to approve or provide review commnent!!'})
 
-    print(f'human_review: {decision}')
-    if(str(decision).strip().lower() in ("approve","approved","yes"," go ahead","good")):
-        print('Approved')
+    if(is_approved(decision)):
         return { 'approved': True, "messages": [AIMessage("APPROVED")]}
-
 
     return { 'approved': False, "messages": [HumanMessage(f"Revise it: {decision}")]}
 
 def route_post_review(state: State, config: RunnableConfig):
-    showState('route_post_review',config)
-    print(f'route_post_review: {result['messages'][-1].text}')
-
     return 'end' if state.get('approved') else 'code_develop'    
 
 # nodes    
@@ -92,20 +77,17 @@ state = State()
 message = { 'approved': False}
 result = app.invoke(message, config=cnf)
 
-
 while(True):
     pause = result['__interrupt__'][0].value
     print(f'PAUSED: {pause}')
 
-    user_approval = input('approved or comment..')
+    user_approval = input('Please approve or provide review comment": ')
 
-    print('Human Review >>')
-    #final = app.invoke(Command(resume=user_approval), config=cnf)
     final = app.invoke(Command(resume=user_approval, update=state), config=cnf)
 
     approval_messgae = final['messages'][-1].text;
 
-    if(str(approval_messgae).strip().lower() in ("approve","approved","yes"," go ahead","good")):
+    if(is_approved(approval_messgae)):
         print('Final after review: ',('*' * 100))
         print(approval_messgae)
         break
